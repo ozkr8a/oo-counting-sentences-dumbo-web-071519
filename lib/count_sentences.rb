6799d426b2a49a47a6049cf38c0846e952e8b5c2
Oscar <oscar_ochoac@yahoo.com>
2019-07-20 22:48:42 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,39 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
+    
+    #split on ""
+    binding.pry
+    arr = self.split(".""?""!")
+    
+    
+    #split on ""
+    #split on ""
 
   end
+  
 end
